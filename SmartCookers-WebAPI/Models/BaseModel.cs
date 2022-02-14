1214c41915ff93ace78696894d509f2142bfb923
pasindu-osadha <pasiduosanda@gmail.com>
2022-02-14 20:29:11 +0530

    update base model

--- SmartCookers-WebAPI/Models/BaseModel.cs
+++ SmartCookers-WebAPI/Models/BaseModel.cs
@@ -2,8 +2,8 @@
 {
     public class BaseModel
     {
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? LastUpdatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }= DateTime.Now;
+        public DateTime? LastUpdatedDate { get; set; }=DateTime.Now;
         public DateTime? DeletedDate { get; set; }
         public bool? IsDeleted { get; set; }
     }
